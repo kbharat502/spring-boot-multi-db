ea6007182ef3feed056a68b18679dd6859c04c01
Bharat K <kbharat502@gmail.com>
2023-05-27 07:58:53 -0400

    Initial commit

---
+++ src/main/java/example/test/springbootmultidb/SpringBootMultiDbApplication.java
@@ -0,0 +1,13 @@
+package example.test.springbootmultidb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultiDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMultiDbApplication.class, args);
+	}
+
+}
